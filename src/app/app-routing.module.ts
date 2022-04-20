9ea75b6c08c087784f56137e87fa553cb11ef8ec
Your Name <you@example.com>
2022-04-20 21:28:13 +0700

    fix liên kết trang, icon of footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { ForstudentComponent } from './forstudent/forstudent.component';
+import { FortutorComponent } from './fortutor/fortutor.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { IntropageComponent } from './intropage/intropage.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '',redirectTo:'/homepage',pathMatch:'full' },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'forstudent', component: ForstudentComponent },
+  { path: 'fortutor', component: FortutorComponent },
+  { path: 'intropage', component: IntropageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
