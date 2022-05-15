c04e86b5080c5241358b7f0c9a66b0dd2799fee7
MyNguyen <nguyenchtm19406c@st.uel.edu.vn>
2022-05-16 02:00:30 +0700

    routerlink footer content

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ExampleService } from '../example.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _activatedRouter: ActivatedRoute,private _router: Router, private _service: ExampleService) { }
 
   ngOnInit(): void {
   }
