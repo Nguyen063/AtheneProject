e5cfd40d6f0e90f7ef44153a2ff2f092798b253c
Your Name <you@example.com>
2022-05-26 21:47:14 +0700

    fix 4learner 4tutor

--- src/app/models/tutor.ts
+++ src/app/models/tutor.ts
@@ -1,48 +1,44 @@
 export class Tutor{
-    TutorInfo=Object({ 
+    LearnerInfo=Object({ 
         Name: String,
         Ava: String,
         Direct: String,
         Gender: String,
         DOB: String,
         CCCD: String,
-        PhoneNumber: String,
-        Exp: String,
-        Degree: String,
-        Education: String,
-        Majoy: String
-    });
-    LearnerFinding=Object({
-    Caphoc: String,
+        PhoneNumber: String});
+    TutorFinding=Object({
+    Level: String,
+    TutorGender: String,
+    class: String,
     Subject: String,
-    LearnerGender: String,
     Location: String,
+    Education: String,
     TeachingTime: String,
     Schedule: String,
+    Request: String, 
     Fee: String,
     Other: String
-     })
+        })
     
-    constructor(){
-        this.TutorInfo.Name="";
-        this.TutorInfo.Ava="";
-        this.TutorInfo.Direct="";
-        this.TutorInfo.Gender="";
-        this.TutorInfo.DOB="";
-        this.TutorInfo.CCCD="";
-        this.TutorInfo.PhoneNumber="";
-        this.TutorInfo.Exp="";
-        this.TutorInfo.Degree="";
-        this.TutorInfo.Education="";
-        this.TutorInfo.Majoy="";
-
-        this.LearnerFinding.Caphoc="";
-        this.LearnerFinding.LearnerGender="";
-        this.LearnerFinding.Subject="";
-        this.LearnerFinding.Location="";
-        this.LearnerFinding.TeachingTime="";
-        this.LearnerFinding.Schedule="";
-        this.LearnerFinding.Fee="";
-        this.LearnerFinding.Other="";
+     constructor( ){
+      this.LearnerInfo.Name="";
+      this.LearnerInfo.Ava="";
+      this.LearnerInfo.Direct="";
+      this.LearnerInfo.Gender="";
+      this.LearnerInfo.DOB="";
+      this.LearnerInfo.CCCD="";
+      this.LearnerInfo.PhoneNumber="";
+     this.TutorFinding.Level="";
+             this.TutorFinding.TutorGender="";
+             this.TutorFinding.class="";
+             this.TutorFinding.Subject="";
+             this.TutorFinding.Location="";
+             this.TutorFinding.Education="";
+             this.TutorFinding.TeachingTime="";
+             this.TutorFinding.Schedule="";
+             this.TutorFinding.Request="";
+             this.TutorFinding.Fee="";
+             this.TutorFinding.Other="";
      }
 }
