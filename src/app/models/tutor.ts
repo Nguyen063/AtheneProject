9b3038e981879a5ef53eb3bbe9f06c26ec551a4d
Maipth <maipth19406c@st.uel.edu.vn>
2022-05-26 16:17:09 +0700

    update tutor data

--- src/app/models/tutor.ts
+++ src/app/models/tutor.ts
@@ -1,11 +1,48 @@
 export class Tutor{
-    _id: any;
-  Name: string;
-  
+    TutorInfo=Object({ 
+        Name: String,
+        Ava: String,
+        Direct: String,
+        Gender: String,
+        DOB: String,
+        CCCD: String,
+        PhoneNumber: String,
+        Exp: String,
+        Degree: String,
+        Education: String,
+        Majoy: String
+    });
+    LearnerFinding=Object({
+    Caphoc: String,
+    Subject: String,
+    LearnerGender: String,
+    Location: String,
+    TeachingTime: String,
+    Schedule: String,
+    Fee: String,
+    Other: String
+     })
+    
+    constructor(){
+        this.TutorInfo.Name="";
+        this.TutorInfo.Ava="";
+        this.TutorInfo.Direct="";
+        this.TutorInfo.Gender="";
+        this.TutorInfo.DOB="";
+        this.TutorInfo.CCCD="";
+        this.TutorInfo.PhoneNumber="";
+        this.TutorInfo.Exp="";
+        this.TutorInfo.Degree="";
+        this.TutorInfo.Education="";
+        this.TutorInfo.Majoy="";
 
-     constructor(){
-         this._id="";
-         this.Name="";
-
+        this.LearnerFinding.Caphoc="";
+        this.LearnerFinding.LearnerGender="";
+        this.LearnerFinding.Subject="";
+        this.LearnerFinding.Location="";
+        this.LearnerFinding.TeachingTime="";
+        this.LearnerFinding.Schedule="";
+        this.LearnerFinding.Fee="";
+        this.LearnerFinding.Other="";
      }
 }
