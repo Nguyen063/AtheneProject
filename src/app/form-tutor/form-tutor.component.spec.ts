ee6273a77b74c02bf7b2c7b3602e16ee69c81b34
Nguyen063 <93272712+Nguyen063@users.noreply.github.com>
2022-05-29 16:43:20 +0700

    THÊM FORM

---
+++ src/app/form-tutor/form-tutor.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormTutorComponent } from './form-tutor.component';
+
+describe('FormTutorComponent', () => {
+  let component: FormTutorComponent;
+  let fixture: ComponentFixture<FormTutorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormTutorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormTutorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
