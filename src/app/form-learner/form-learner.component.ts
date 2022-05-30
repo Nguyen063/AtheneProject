3db9cca7ca12b1e87b731e75cc10523f821c7032
MyNguyen <nguyenchtm19406c@st.uel.edu.vn>
2022-05-30 19:59:25 +0700

    Set Validation for form-learner and form-tutor

--- src/app/form-learner/form-learner.component.ts
+++ src/app/form-learner/form-learner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-learner',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-learner.component.css']
 })
 export class FormLearnerComponent implements OnInit {
-
-  constructor() { }
+  public form:any;
+  constructor(private _formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.form=this._formBuilder.group({
+      ho:['', Validators.required],
+      ten:['', Validators.required],
+      thanhpho:['', Validators.required],
+      sonha:['', Validators.required],
+      duong:['', Validators.required],
+      phuong:['', Validators.required],
+      quan:['', Validators.required],
+      dob:['', Validators.required],
+      //salary:['',  [Validators.required, Validators.pattern("^[0-9]*$")]],
+      phone:['',[Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(/((09|03|07|08|05)+[0-9]{8}\b)/g)]],
+      ngaycap:['', Validators.required],
+      cccd:['',[Validators.required, Validators.maxLength(12), Validators.minLength(12), Validators.pattern(/((0)+[0-9]{11}\b)/g)]]
   }
+  )}
+  // Get input phone
+ get phone(){
+  return this.form.controls['phone']}
 
+  // Get input cccd
+ get cccd(){
+  return this.form.controls['cccd']}
+  
+  //Submit form
+  onSubmit() {
+  
+    // stop here if form is invalid
+    if (this.form.invalid) {
+        return console.log(this.form.value)
+    }
+    alert('Bạn đã đăng kí thành công tài khoản, vui lòng đăng nhập lại ^_^')
+   
+  }
+  
 }
--- src/app/form-tutor/form-tutor.component.ts
+++ src/app/form-tutor/form-tutor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-tutor',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-tutor.component.css']
 })
 export class FormTutorComponent implements OnInit {
-
-  constructor() { }
+  public form:any;
+  constructor(private _formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.form=this._formBuilder.group({
+      ho:['', Validators.required],
+      ten:['', Validators.required],
+      thanhpho:['', Validators.required],
+      sonha:['', Validators.required],
+      duong:['', Validators.required],
+      phuong:['', Validators.required],
+      quan:['', Validators.required],
+      dob:['', Validators.required],
+      major:['', Validators.required],
+      //salary:['',  [Validators.required, Validators.pattern("^[0-9]*$")]],
+      phone:['',[Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(/((09|03|07|08|05)+[0-9]{8}\b)/g)]],
+      ngaycap:['', Validators.required],
+      cccd:['',[Validators.required, Validators.maxLength(12), Validators.minLength(12), Validators.pattern(/((0)+[0-9]{11}\b)/g)]]
+  })
 
+}
+// Get input phone
+get phone(){
+  return this.form.controls['phone']}
+
+  // Get input cccd
+ get cccd(){
+  return this.form.controls['cccd']}
+  
+  //Submit form
+  onSubmit() {
+  
+    // stop here if form is invalid
+    if (this.form.invalid) {
+        return console.log(this.form.value)
+    }
+    alert('SUCCESS!! ^_^')
+   
+  }
 }
