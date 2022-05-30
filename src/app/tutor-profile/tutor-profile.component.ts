94abdc07c3c6d3ec1c307edb8b88bd040a1b4d64
tanthanh2001 <thanhhtt19406c@st.uel.edu.vn>
2022-05-30 21:55:50 +0700

    update tutor profile

--- src/app/tutor-profile/tutor-profile.component.ts
+++ src/app/tutor-profile/tutor-profile.component.ts
@@ -17,53 +17,6 @@ export class TutorProfileComponent implements OnInit {
   constructor(private _service:ProfileService) { }
 
   ngOnInit(): void {
-    this.getProfiles()
   }
-  getProfiles(){
-    this._service.getProfiles().subscribe({
-      next:data => this.profiles=data,
-      error : err=> this.errMessage=err
-    })
-   }
 
-   openEditProfile(data: Profile){
-    this.open=!this.open
-    this.profile =data;
- }
- closeEdit(){
-  this.open=!this.open
- }
- // Save
-// submitData(form: NgForm){
-
-//   if(this.profile._id==""){
-//     this._service.postProfile(this.profile).subscribe(res =>{
-//       let resData=JSON.parse(JSON.stringify(res));
-//       if(resData.message ==="success"){
-//         // alert("Success");
-//         this.getProfiles();
-
-//       }else{alert("Failed")
-//     } 
-//     })
-//   } else{
-//     this._service.updateProfile(this.profile._id,this.profile).subscribe(res =>{
-//       let resData=JSON.parse(JSON.stringify(res));
-//       if(resData.message ==="success"){
-//         //alert("Updated Successfully");
-//         this.getProfiles();
-
-//       }else alert("Failed")
-//     })
-//   }
-//   }
-//   edit(data: Profile){
-//     console.log(data);
-//     this.profile =data
-//   }
-//   onReset(form?:NgForm){
-//     if(form)
-//     form.reset();
-//     this.profile=new Profile();
-//   }
 }
